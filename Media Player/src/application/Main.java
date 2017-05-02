8c599b3c4bb28a682ed7e6428c1e4d9a11f6a4bd
Ryane12 <Ryan.P.Earley-1@ou.edu>
2017-05-01 23:03:36 -0500

    Project

--- Media Player/src/application/Main.java
+++ Media Player/src/application/Main.java
@@ -1,26 +1,34 @@
 package application;
 	
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public void start(Stage primaryStage) {
+        try {
+            BorderPane page = (BorderPane) FXMLLoader.load(Main.class.getResource("Scene.fxml"));
+            //Controller cont = new Controller(primaryStage);
+            Scene scene = new Scene(page);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("My Media");
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
+
